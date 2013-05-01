f01fb3528aeb15b0afd63683905cf733acab1f17
GulinSS <gulin.serge@gmail.com>
2013-05-01 23:59:22 +0400

    tests fixed for blocks

---
+++ test/test_blocklines.js
@@ -0,0 +1,129 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: GulinSS
+ * Date: 01.05.13
+ * Time: 22:57
+ */
+
+var eng = require('../lib/drawingengine.js');
+var grid = require('../lib/grid_utils.js');
+
+var test = require('tap').test;
+
+function do_test(input,output,moves) {
+  var model = {
+    gridRows : input.length,
+    gridCols : input[0].length,
+    type : 'block'
+  };
+  var init_state = eng.reset(model);
+  init_state.lines = grid.from_string(input.join('\n'));
+  var state = eng.move(model,init_state, moves);
+  return [grid.to_string(state.lines) , output.join('\n')];
+}
+
+/*test('drawing cross', function(t) {
+  t.plan(1);
+
+  var input = [
+    ' ┌┐',
+  ];
+  var output = [
+    '┌ ',
+    '└┘'
+  ];
+  var moves = 'R: :1,D:#:1';
+
+  var l = do_test(input,output,moves);
+  t.equal(l[0],l[1]);
+  t.end();
+});
+*/
+
+test('drawing a 2x2 square', function (t) {
+  t.plan(1);
+
+//   0123456789012345678901234567890123456789012345678901234567890123456789
+  var input = [
+    '                   ',
+    '                   ',
+    '                   ',
+    '                   '
+  ];
+  var output = [
+    '                   ',
+    '          ┌┐       ',
+    '          └┘       ',
+    '                   '
+  ];
+  var moves = 'D: :1,R: :10,R:-:1,D:-:1,L:-:1,U:-:1';
+
+  var l = do_test(input,output,moves);
+  t.equal(l[0],l[1]);
+  t.end();
+});
+
+test('erasing top half a 2x2 square', function (t) {
+  t.plan(1);
+
+//   0123456789012345678901234567890123456789012345678901234567890123456789
+  var input = [
+    '                   ',
+    '          ┌┐       ',
+    '          └┘       ',
+    '                   '
+  ];
+  var output = [
+    '                   ',
+    '                   ',
+    '          └┘       ',
+    '                   '
+  ];
+  var moves = 'D: :1,R: :10,R:#:2';
+
+  var l = do_test(input,output,moves);
+  t.equal(l[0],l[1]);
+  t.end();
+});
+
+test('erasing left half a 2x2 square', function (t) {
+  t.plan(1);
+
+//   0123456789012345678901234567890123456789012345678901234567890123456789
+  var input = [
+    '                   ',
+    '          ┌┐       ',
+    '          └┘       ',
+    '                   '
+  ];
+  var output = [
+    '                   ',
+    '           ┐       ',
+    '           ┘       ',
+    '                   '
+  ];
+  var moves = 'D: :1,R: :10,D:#:2';
+
+  var l = do_test(input,output,moves);
+  t.equal(l[0],l[1]);
+  t.end();
+});
+
+ test('writing/erasing at edge of buffer', function (t) {
+  t.plan(1);
+
+//   0123456789012345678901234567890123456789012345678901234567890123456789
+  var input = [
+    '┌┐',
+    '└┘'
+  ];
+  var output = [
+    '┌ ',
+    '└┘'
+  ];
+  var moves = 'R: :1,D:#:1';
+
+  var l = do_test(input,output,moves);
+  t.equal(l[0],l[1]);
+  t.end();
+});
