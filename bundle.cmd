e4b5b6115b18588ed01303237776a926b4620abb
Maurits Meijer <meijer20@gmail.com>
2013-05-23 00:01:14 +0200

    added windows scripts to run bundle, cover and test

---
+++ bundle.cmd
@@ -0,0 +1,1 @@
+node_modules/.bin/browserify lib/main.js -o static/bundle.js
---
+++ cover.cmd
@@ -0,0 +1,3 @@
+node_modules/.bin/cover run alltests.js
+node_modules/.bin/cover report
+node_modules/.bin/cover report html
